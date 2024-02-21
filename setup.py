365b0c094baae251fcf7b7a4718b2502260debf4
mayankchugh1977 <mayank.chugh@gmail.com>
2024-02-22 01:46:29 +0800

    folder structure  commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Mayank Chugh",
+    author_email="mayankchugh.learning@gmail.com",
+    packages=find_packages()
+)
